f0e87485ce82e39f5e1a342d094a1b935572fb56
Gabrielly Santos <gabriellysantos@live.com>
2026-04-16 18:37:18 -0300

    ♻️ refactor: adiciona tipagem as váriaveis

--- src/1000-1099/1001.ts
+++ src/1000-1099/1001.ts
@@ -3,8 +3,8 @@ import * as fs from "fs";
 const input = fs.readFileSync(0, "utf8");
 const lines = input.split(/\r?\n/);
 
-const A = parseInt(lines.shift()!);
-const B = parseInt(lines.shift()!);
+const A: number = parseInt(lines.shift()!);
+const B: number = parseInt(lines.shift()!);
 
 const X = A + B;
 
